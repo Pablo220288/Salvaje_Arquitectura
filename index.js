86f068d544c39b6a91dc47dccbb6010dfe1ecb3e
Pab Her <peh_tj@hotmail.com>
2023-10-24 10:32:46 -0300

    Modal Contact v1

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 //Loader
-window.addEventListener('load', () => {
-    document.getElementById('loader').classList.toggle('loader-hide');
-  });
-  
+window.addEventListener("load", () => {
+  document.getElementById("loader").classList.toggle("loader-hide");
+});
+
+const mail = document.querySelector(".mail");
+const contactModal = document.querySelector(".contactModal");
+const btnCancel = document.querySelector(".btnCancel");
+
+mail.addEventListener("click", () => {
+  contactModal.classList.add("contactModalShow");
+});
+
+btnCancel.addEventListener("click", (e) => {
+  contactModal.classList.remove("contactModalShow");
+});
