b6f78629670b381feb0cffb0bcd452703e382309
Pab Her <peh_tj@hotmail.com>
2023-10-24 12:34:11 -0300

    Footer

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ btnCancel.addEventListener("click", (e) => {
   contactModal.classList.remove("contactModalShow");
 });
  */
+
+const textarea = document.querySelector(".textarea");
+
+textarea.addEventListener("keydown", autosize);
+
+function autosize() {
+  let el = this;
+  setTimeout(function () {
+    el.style.cssText = "height:auto; padding:0";
+    el.style.cssText = "height:" + el.scrollHeight + "px";
+  }, 0);
+}
